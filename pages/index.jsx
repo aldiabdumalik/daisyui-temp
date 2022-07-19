af38fc3b81601fe3485b75f130dcec7e8907648c
Abdu <aldiabdumalik98@gmail.com>
2022-07-19 14:37:06 +0700

    module icon added

--- pages/index.jsx
+++ pages/index.jsx
@@ -1,5 +1,6 @@
 import Head from 'next/head'
-import Image from 'next/image'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { solid, regular, brands } from '@fortawesome/fontawesome-svg-core/import.macro'
 
 export default function Home() {
   return (
@@ -7,6 +8,7 @@ export default function Home() {
       <Head>
         <title>Create Next App</title>
       </Head>
+      <div><FontAwesomeIcon icon={brands('twitter')} size="6x" /></div>
     </div>
   )
 }
