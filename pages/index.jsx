b4fa83aa5d2f96820a696d645d022a370b539399
Abdu <aldiabdumalik98@gmail.com>
2022-07-19 13:22:07 +0700

    config init

--- pages/index.jsx
+++ pages/index.jsx
@@ -1,9 +1,12 @@
 import Head from 'next/head'
 import Image from 'next/image'
-import styles from '../styles/Home.module.css'
 
 export default function Home() {
   return (
-    <button class="btn btn-rounded">Button</button>
+    <div>
+      <Head>
+        <title>Create Next App</title>
+      </Head>
+    </div>
   )
 }
--- tailwind.config.js
+++ tailwind.config.js
@@ -6,5 +6,22 @@ module.exports = {
     theme: {
       extend: {},
     },
+    daisyui: {
+      themes: [
+        {
+          tempcust: {
+            "primary": "#FF1818",
+            "secondary": "#172774",
+            "accent": "#ebf43a",
+            "neutral": "#090910",
+            "base-100": "#F0F0F0",
+            "info": "#88E0EF",
+            "success": "#207E4A",
+            "warning": "#FFC300",
+            "error": "#FF6B6B",
+          },
+        },
+      ],
+    },
     plugins: [require("daisyui")],
 }
